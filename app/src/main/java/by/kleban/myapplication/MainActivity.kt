d88dfdd0f8391b91479aec18f5406a454b809ee9
Ella Kleban <ella.kleban@gmail.com>
2021-04-10 14:14:48 +0300

    homework1

--- app/src/main/java/by/kleban/myapplication/MainActivity.kt
+++ app/src/main/java/by/kleban/myapplication/MainActivity.kt
@@ -1,11 +1,20 @@
 package by.kleban.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonDz1 = findViewById<Button>(R.id.dz1)
+
+        buttonDz1.setOnClickListener {
+
+            startActivity(Intent(this, ImageUploadActivity::class.java))
+        }
     }
 }
