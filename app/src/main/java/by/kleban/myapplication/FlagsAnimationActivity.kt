ed49cc128587368df0c62dad783e70175329b9c2
Ella Kleban <ella.kleban@gmail.com>
2021-04-12 23:41:23 +0300

    Create FlagsAnimationActivity and layout

---
+++ app/src/main/java/by/kleban/myapplication/FlagsAnimationActivity.kt
@@ -0,0 +1,13 @@
+package by.kleban.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class FlagsAnimationActivity:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flags_animation)
+    }
+}
--- app/src/main/java/by/kleban/myapplication/ImageUploadActivity.kt
+++ app/src/main/java/by/kleban/myapplication/ImageUploadActivity.kt
@@ -10,6 +10,7 @@ import com.squareup.picasso.Picasso
 
 
 class ImageUploadActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_upload)
