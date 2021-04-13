f4119d2aaa7e732641ea7daa8917b0cadcbe5306
Ella Kleban <ella.kleban@gmail.com>
2021-04-13 11:41:42 +0300

    Create FlagsActivity

--- app/src/main/java/by/kleban/myapplication/FlagsAnimationActivity.kt
+++ app/src/main/java/by/kleban/myapplication/FlagsAnimationActivity.kt
@@ -6,15 +6,22 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
-class FlagsAnimationActivity:AppCompatActivity() {
+class FlagsAnimationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flags_animation)
 
         val buttonAnimation = findViewById<Button>(R.id.btn_animation)
+        val buttonFlags = findViewById<Button>(R.id.btn_flags)
+
         buttonAnimation.setOnClickListener {
             val intent = Intent(this, CatAnimationActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonFlags.setOnClickListener {
+            val intent = Intent(this, FlagsActivity::class.java)
             startActivity(intent)
         }
     }
