56a84b1903188a94b020bf7a782e4153fef597be
Ella Kleban <ella.kleban@gmail.com>
2021-04-16 19:47:02 +0300

    Create BelarusSowingActivity

---
+++ app/src/main/java/by/kleban/myapplication/BelarusSowingActivity.kt
@@ -0,0 +1,12 @@
+package by.kleban.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class BelarusSowingActivity :AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
