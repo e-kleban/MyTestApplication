218ac8c6b8a8a7f986f79fe00176c5ee950cd45a
Ella Kleban <ella.kleban@gmail.com>
2021-05-15 13:40:40 +0300

    create fragments

---
+++ app/src/main/java/by/kleban/myapplication/homework8/WelcomeCryptoFragment.kt
@@ -0,0 +1,20 @@
+package by.kleban.myapplication.homework8
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import by.kleban.myapplication.R
+
+
+class WelcomeCryptoFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_welcome_crypto,container,false)
+    }
+}
