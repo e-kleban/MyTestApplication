a86a3b64674263198ce8aefb9c5fc23e933d0b47
Ella Kleban <ella.kleban@gmail.com>
2021-05-24 16:08:56 +0300

    change

--- app/src/main/java/by/kleban/myapplication/homework9/MyForegroundService.kt
+++ app/src/main/java/by/kleban/myapplication/homework9/MyForegroundService.kt
@@ -4,7 +4,9 @@ import android.app.Notification
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.os.Bundle
 import android.os.IBinder
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import by.kleban.myapplication.R
 
@@ -17,17 +19,21 @@ class MyForegroundService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
-        val intentService = Intent(this, MysteryFinalActivity::class.java)
-        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intentService, 0)
+        val intentService = Intent(this, MysteryFinalActivity::class.java).
+         putExtra(MysteryFinalActivity.EXTRA_FINAL_STRING,intent?.extras?.getString(EXTRA_STRING))
+
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intentService, PendingIntent.FLAG_CANCEL_CURRENT
+        )
 
         val notification = NotificationCompat.Builder(this, MyApp.CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_baseline_compost_24)
             .setContentTitle("Advice")
-            .setContentText(intent?.getStringExtra(EXTRA_STRING))
+            .setContentText("You have new message")
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setCategory(Notification.CATEGORY_SERVICE)
             .setStyle(NotificationCompat.BigTextStyle())
             .setContentIntent(pendingIntent)
+            .addAction(R.drawable.ic_baseline_compost_24,"open",pendingIntent)
             .build()
 
         startForeground(1, notification)
