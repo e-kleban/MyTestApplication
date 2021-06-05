d4f3fb4ce34edb02748228fcc5a336dc182f8fa6
Ella Kleban <ella.kleban@gmail.com>
2021-06-05 23:29:31 +0300

    correct main activity

--- app/src/main/java/by/kleban/myapplication/MainActivity.kt
+++ app/src/main/java/by/kleban/myapplication/MainActivity.kt
@@ -4,9 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import by.kleban.myapplication.databinding.ActivityMainBinding
 import by.kleban.myapplication.homework1.ImageUploadActivity
 import by.kleban.myapplication.homework11.CountryContainerActivity
-import by.kleban.myapplication.homework11.CountryHolidayFragment
 import by.kleban.myapplication.homework2.FlagsAnimationActivity
 import by.kleban.myapplication.homework3.BelarusSowingActivity
 import by.kleban.myapplication.homework4.SnowdropStartActivity
@@ -18,67 +18,60 @@ import by.kleban.myapplication.homework9.MysteryContainerActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val buttonDz1 = findViewById<Button>(R.id.dz1)
-        val buttonDz2 = findViewById<Button>(R.id.dz2)
-        val buttonDz3 = findViewById<Button>(R.id.dz3)
-        val buttonDz4 = findViewById<Button>(R.id.dz4)
-        val buttonDz5 = findViewById<Button>(R.id.dz5)
-        val buttonDz6 = findViewById<Button>(R.id.dz6)
-        val buttonDz7 = findViewById<Button>(R.id.dz7)
-        val buttonDz8 = findViewById<Button>(R.id.dz8)
-        val buttonDz9 = findViewById<Button>(R.id.dz9)
-        val buttonDz11 = findViewById<Button>(R.id.dz11)
-
-        buttonDz1.setOnClickListener {
+        binding.dz1.setOnClickListener {
             val intent = Intent(this, ImageUploadActivity::class.java)
             startActivity(intent)
         }
 
-        buttonDz2.setOnClickListener {
+        binding.dz2.setOnClickListener {
             val intent = Intent(this, FlagsAnimationActivity::class.java)
             startActivity(intent)
         }
 
-        buttonDz3.setOnClickListener {
+        binding.dz3.setOnClickListener {
             val intent = Intent(this, BelarusSowingActivity::class.java)
             startActivity(intent)
         }
 
-        buttonDz4.setOnClickListener {
+        binding.dz4.setOnClickListener {
             val intent = Intent(this, SnowdropStartActivity::class.java)
             startActivity(intent)
         }
 
-        buttonDz5.setOnClickListener {
+        binding.dz5.setOnClickListener {
             val intent = Intent(this, UserRegistrationActivity::class.java)
             startActivity(intent)
         }
 
-        buttonDz6.setOnClickListener {
+        binding.dz6.setOnClickListener {
             val intent = Intent(this, CongratulationContainerActivity::class.java)
             startActivity(intent)
         }
 
-        buttonDz7.setOnClickListener {
+        binding.dz7.setOnClickListener {
             val intent = Intent(this, DogContainerActivity::class.java)
             startActivity(intent)
         }
 
-        buttonDz8.setOnClickListener {
+        binding.dz8.setOnClickListener {
             val intent = Intent(this, ContainerHw8Activity::class.java)
             startActivity(intent)
         }
 
-        buttonDz9.setOnClickListener {
+        binding.dz9.setOnClickListener {
             val intent = Intent(this, MysteryContainerActivity::class.java)
             startActivity(intent)
         }
 
-        buttonDz11.setOnClickListener {
+        binding.dz11.setOnClickListener {
             val intent = Intent(this, CountryContainerActivity::class.java)
             startActivity(intent)
         }
